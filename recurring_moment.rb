4f199b5830026809a2645c7ef0234d2bb2c635ff
mmorales4050 <mmorales4050@gmail.com>
2019-05-21 10:51:24 -0400

    tests passing

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'pry'
 
 class RecurringMoment
   def initialize(start:, interval:, period:)
@@ -9,20 +10,25 @@ class RecurringMoment
   end
 
   def match(date)
+    counter = @interval
     current = @start
 
-    while current < date
+    while current <= date
       if current == date
         return true
       end
 
       if @period == 'monthly'
+        current = @start.advance(months: @interval)
+        if current != @start.advance(months: counter)
+          current = @start.advance(months: counter)
+        end
+      elsif @period == 'weekly'
         current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
-        current = current.advance(months: @interval)
       elsif @period == 'daily'
         current = current.advance(days: @interval)
       end
+      counter += @interval
     end
 
     return false
